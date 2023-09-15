f64ba0feb48ec9fcd4e58688b641cef3fe8e81ce
Teodora Pandeva <t.p.pandeva@uva.nl>
2023-09-15 11:21:40 +0200

    mnist experiment

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -49,6 +49,9 @@ class Trainer:
 
     def train_evaluate_epoch(self, loader, mode="train"):
         """Train/Evaluate the model for one epocj and log the results."""
+        aggregated_loss = 0
+        mmde = 1
+        num_samples = len(loader.dataset)
         for i, z in enumerate(loader):
             z = z.to(self.device)
             if mode == "train":
@@ -56,26 +59,29 @@ class Trainer:
             else:
                 out = self.net(z, self.operator).detach()
             loss = -out.mean()
-            mmde = torch.exp(out.sum())
+            aggregated_loss +=-out.sum()
+            mmde *= torch.exp(out.sum())
             if mode == "train":
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-        self.log({f"{mode}_eval": mmde.item(), f"{mode}_loss": loss.item()})
-        return loss, mmde
+        self.log({f"{mode}_eval": mmde.item(), f"{mode}_loss": aggregated_loss.item()/num_samples})
+        return aggregated_loss/num_samples, mmde
 
-    def load_data(self, seed):
+    def load_data(self, seed, mode= "train"):
         """Load data using the datagen object and return a DataLoader object."""
         data = self.datagen.generate(seed)
-        data_loader = DataLoader(data, batch_size=self.bs, shuffle=True)
+        if mode in ["train", "val"]:
+            data_loader = DataLoader(data, batch_size=self.bs, shuffle=True)
+        else: data_loader = DataLoader(data, batch_size=len(data), shuffle=True)
         return data, data_loader
 
     def train(self):
         """Train the model for a specified number of sequences, epochs, and apply early stopping if required."""
         torch.manual_seed(self.seed)
         np.random.seed(self.seed)
-        train_data, train_loader = self.load_data(self.seed)
-        val_data, val_loader = self.load_data(self.seed + 1)
+        train_data, train_loader = self.load_data(self.seed, mode = "train")
+        val_data, val_loader = self.load_data(self.seed + 1, mode = "val")
         mmdes = []
         for k in range(self.seqs):
 
@@ -83,13 +89,15 @@ class Trainer:
                 self.train_evaluate_epoch(train_loader)
                 loss_val, _ = self.train_evaluate_epoch(val_loader, mode='val')
                 if self.early_stopper.early_stop(loss_val.detach()) or (t + 1) == self.epochs:
-                    test_data, test_loader = self.load_data(self.seed + k + 2)
+                    test_data, test_loader = self.load_data(self.seed + k + 2, mode = "test")
                     _, mmde_conditional = self.train_evaluate_epoch(test_loader, mode='test')
                     mmdes.append(mmde_conditional.item())
                     mmde = np.prod(np.array(mmdes[self.T:])) if k >= self.T else 1
                     self.log({"aggregated_test_eval": mmde})
                     train_data = ConcatDataset([train_data, val_data])
                     val_data = test_data
+                    train_loader = DataLoader(train_data, batch_size=self.bs, shuffle=True)
+                    val_loader = DataLoader(val_data, batch_size=self.bs, shuffle=True)
                     self.log({"iterations": t})
                     break
             self.early_stopper.reset()
