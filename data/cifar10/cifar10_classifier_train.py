095903a7a7ea6c4ed52c1ab31088fef706d912f2
Teodora Pandeva <t.p.pandeva@uva.nl>
2023-09-19 13:38:02 +0200

    cifar10

--- data/cifar10/cifar10_classifier_train.py
+++ data/cifar10/cifar10_classifier_train.py
@@ -8,19 +8,29 @@ import torchvision.models as models
 
 if __name__ == '__main__':
     # download and load training dataset
-    transform = transforms.Compose(
-        [transforms.ToTensor(),
-         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
 
     batch_size = 64
 
     trainset = torchvision.datasets.CIFAR10(root='./cifar10', train=True,
-                                            download=True, transform=transform)
+                                            download=True, transform=transform_train)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
                                               shuffle=True, num_workers=1)
 
     testset = torchvision.datasets.CIFAR10(root='./cifar10', train=False,
-                                           download=True, transform=transform)
+                                           download=True, transform=transform_test)
     testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size,
                                              shuffle=False, num_workers=1)
 
