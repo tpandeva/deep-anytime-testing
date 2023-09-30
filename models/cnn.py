6c5ff11cde75172c55a82d2ebd2e14acd6d08600
Teodora Pandeva <t.p.pandeva@uva.nl>
2023-09-30 18:13:09 +0200

    mnist

--- models/cnn.py
+++ models/cnn.py
@@ -6,20 +6,36 @@ class CNN(nn.Module):
         super().__init__()
         self.conv = nn.Sequential(nn.Conv2d(input_size, 32, 3, 1),
                                   nn.ReLU(),
-                                  nn.Conv2d(32, 64, 3, 1),
-                                  nn.ReLU(),
                                   nn.MaxPool2d(2),
                                   nn.Dropout(0.25))
-        self.fc = nn.Sequential(nn.Linear(9216, 128),
+        self.fc = nn.Sequential(nn.Linear(9216, 100),
                                 nn.ReLU(),
                                 nn.Dropout(0.5),
-                                nn.Linear(128, output_size))
+                                nn.Linear(100, output_size))
 
     def forward(self, x):
         x = self.conv(x)
         x = torch.flatten(x, 1)
         x = self.fc(x)
         return x
+class CNNMMDE(CNN):
+    def __init__(self, input_size, output_size):
+        super().__init__(input_size, output_size)
+        self.sigma = torch.nn.Tanh()
+    def forward(self, x, y):
+
+        num_rotations = x.shape[1] if len(x.shape) == 5 else 1
+        g_x, g_y = 0, 0
+        for i in range(num_rotations):
+            x_conv = self.conv(x[:,i,...])
+            out_x = torch.flatten(x_conv, 1)
+            g_x += self.fc(out_x)
+            y_conv = self.conv(y[:,i,...])
+            out_y = torch.flatten(y_conv, 1)
+            g_y += self.fc(out_y)
+        output = torch.log(1 + self.sigma(g_x - g_y))
+        return output
+
 
 class RotCNNMMDE(CNN):
     def __init__(self, input_size, output_size):
