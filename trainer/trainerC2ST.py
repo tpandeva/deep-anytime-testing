46f9c85348e0701525373a80e6cddd96c87a3431
Teodora Pandeva <t.p.pandeva@uva.nl>
2023-10-10 14:43:20 +0200

    clean train functions

--- trainer/trainerC2ST.py
+++ trainer/trainerC2ST.py
@@ -5,57 +5,103 @@ import torch
 class TrainerC2ST(Trainer):
 
     def __init__(self, cfg, net, tau1, tau2, datagen, device, data_seed):
-        """Initializes the Trainer object with the provided configurations and parameters."""
+        """
+        Initializes the TrainerC2ST object by extending the Trainer class.
+
+        Args:
+        (same as Trainer class)
+        """
         super().__init__(cfg, net, tau1, tau2, datagen, device, data_seed)
         self.loss = torch.nn.CrossEntropyLoss(reduction='sum')
-        self.opt_lmbd = 0
-        self.run_mean = 0
-        self.grad_sq_sum = 1
-        self.truncation_level = 0.5
+        self.opt_lmbd = 0  # betting strategy parameter
+        self.run_mean = 0  # Running mean of payoffs
+        self.grad_sq_sum = 1  # Sum of squares of gradients
+        self.truncation_level = 0.5  # Level for truncating optimization
 
     def testing_by_betting(self, y, logits):
-        # ONS betting strategy
+        """
+        Implement the ONS betting strategy.
+
+        Args:
+        - y (torch.Tensor): Ground truth labels.
+        - logits (torch.Tensor): Model outputs before activation.
+
+        Returns:
+        - tuple: E-value and E-value using ONS betting strategy.
+        """
+        # Calculate the weights and transformed probabilities using softmax
         w = 2 * y - 1
-        f = torch.nn.Softmax()
+        f = torch.nn.Softmax(dim=1)
         ft = 2 * f(logits)[:, 1] - 1
         e_val = torch.exp(torch.sum(torch.log(1 + w * ft)))
         payoffs = w * ft
 
+        # Update optimization parameters using gradient information
         grad = self.run_mean / (1 + self.run_mean * self.opt_lmbd)
         self.grad_sq_sum += grad ** 2
         self.opt_lmbd = max(0, min(
             self.truncation_level, self.opt_lmbd + 2 / (2 - np.log(3)) * grad / self.grad_sq_sum))
         e_val_ons = torch.exp(torch.log(1 + self.opt_lmbd * payoffs.sum()))
         self.run_mean = payoffs.mean()
+
         return e_val, e_val_ons
 
     def e_c2st(self, y, logits):
+        """
+        Evaluate the E-C2ST for given targets and logits.
 
-        # H0
-        # p(y|x) of MLE under H0: p(y|x) = p(y), is just the empirical frequency of y in the test data.
+        Args:
+        - y (torch.Tensor): Ground truth labels.
+        - logits (torch.Tensor): Model outputs before activation.
+
+        Returns:
+        - torch.Tensor: Evaluated E-value.
+        """
+        # H0: Empirical frequencies
         emp_freq_class0 = 1 - (y[y == 1]).sum() / y.shape[0]
         emp_freq_class1 = (y[y == 1]).sum() / y.shape[0]
 
-        # H1
-        # Probabilities under empirical model (using train data)
-        f = torch.nn.Softmax()
+        # H1: Probabilities under empirical model (using train data)
+        f = torch.nn.Softmax(dim=1)
         prob = f(logits)
         pred_prob_class0 = prob[:, 0]
         pred_prob_class1 = prob[:, 1]
         log_eval = torch.sum(y * torch.log(pred_prob_class1 / emp_freq_class1) + (1 - y) * torch.log(
             pred_prob_class0 / emp_freq_class0)).double()
         eval = torch.exp(log_eval)
-        # E-value
+
         return eval
 
     def first_k_unique_permutations(self, n, k):
-        if np.log(k) > n * (np.log(n) - 1) + 0.5 * (np.log(2 * np.pi * n)): k = n
+        """
+        Generate the first k unique permutations of range(n).
+
+        Args:
+        - n (int): Size of the set.
+        - k (int): Number of unique permutations required.
+
+        Returns:
+        - list: List of k unique permutations.
+        """
+        if np.log(k) > n * (np.log(n) - 1) + 0.5 * (np.log(2 * np.pi * n)):
+            k = n
         unique_perms = set()
         while len(unique_perms) < k:
             unique_perms.add(tuple(np.random.choice(n, n, replace=False)))
         return list(unique_perms), k
 
     def s_c2st(self, y, logits, n_per=100):
+        """
+        Evaluate the permutation-based Two-Sample Test (TST) for given labels and logits.
+
+        Args:
+        - y (torch.Tensor): Ground truth labels.
+        - logits (torch.Tensor): Model outputs before activation.
+        - n_per (int, optional): Number of permutations. Default is 100.
+
+        Returns:
+        - tuple: p-value and accuracy.
+        """
         y_hat = torch.argmax(logits, dim=1)
         n = y.shape[0]
         accuracy = torch.sum(y == y_hat) / n
@@ -63,16 +109,24 @@ class TrainerC2ST(Trainer):
         permutations, n_per = self.first_k_unique_permutations(n, n_per)
         for r in range(n_per):
             ind = np.asarray(permutations[r])
-            # divide into new X, Y
             y_perm = y.clone()[ind]
-            # compute accuracy
             stats[r] = torch.sum(y_perm == y_hat) / y.shape[0]
         sorted_stats = np.sort(stats)
         p_val = np.sum(sorted_stats > accuracy.item()) / n_per
+
         return p_val, accuracy
 
     def train_evaluate_epoch(self, loader, mode="train"):
-        """Train/Evaluate the model for one epocj and log the results."""
+        """
+        Train/Evaluate the model for one epoch using the C2ST approach.
+
+        Args:
+        - loader (DataLoader): DataLoader object to iterate through data.
+        - mode (str): Either "train" or "test". Determines how to run the model.
+
+        Returns:
+        - tuple: Aggregated loss and E-value using ONS betting strategy for the current epoch.
+        """
         aggregated_loss = 0
         e_val, tb_val, tb_val_ons = 1, 1, 1
         num_samples = len(loader.dataset)
@@ -92,32 +146,42 @@ class TrainerC2ST(Trainer):
                 self.device)
             loss = self.loss(out, labels)
             aggregated_loss += loss
+
             if mode == "train":
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
-            # compute e-c2st, seq-it and s-c2st
+
+            # Compute the C2ST and other evaluation metrics
             if mode == "test":
                 e_val *= self.e_c2st(labels, out.detach())
-
-                p_val, acc = self.s_c2st(labels, out.detach(), n_per=100)
-
+                p_val, acc = self.s_c2st(labels, out.detach())
                 results_tb = self.testing_by_betting(labels, out.detach())
-                tb_val *= results_tb[0]
-                tb_val_ons *= results_tb[1]
-                self.log(
-                    {f"{mode}_e-value": e_val.item(), f"{mode}_p-value": p_val.item(),
-                     f"{mode}_tb-value": tb_val.item(), f"{mode}_tb-ons-value": tb_val_ons.item(),
-                    })
-            self.log({ f"{mode}_loss": aggregated_loss.item() / num_samples, f"{mode}_accuracy": acc.item() })
+                tb_val_ons = results_tb[1]
+
+                self.log({
+                    f"{mode}_p-value": p_val,
+                    f"{mode}_accuracy": acc
+                })
+
+            self.log({
+                f"{mode}_loss": aggregated_loss.item() / num_samples
+            })
+
         return aggregated_loss / num_samples, tb_val_ons
 
 # Only for the mnist experiments
 class TrainerSC2ST(TrainerC2ST):
 
-
     def __init__(self, cfg, net, tau1, tau2, datagen, device, data_seed):
-        """Initializes the Trainer object with the provided configurations and parameters."""
+        """
+        Initializes the TrainerSC2ST object by extending the TrainerC2ST class.
+
+        This variant is specific for MNIST experiments with multiple network models.
+
+        Args:
+        (same as TrainerC2ST class)
+        """
         super().__init__(cfg, net[0], tau1, tau2, datagen, device, data_seed)
         param = []
         self.net = net
@@ -126,54 +190,65 @@ class TrainerSC2ST(TrainerC2ST):
         self.optimizer = torch.optim.Adam(param, lr=cfg.lr)
         self.loss = torch.nn.CrossEntropyLoss(reduction='sum')
 
-    def s_c2st(self, y, logits, n_per=100):
-        y_hat = torch.argmax(logits, dim=1)
-        n = y.shape[0]
-        accuracy = torch.sum(y == y_hat) / n
-        stats = np.zeros(n_per)
-        permutations, n_per = self.first_k_unique_permutations(n, n_per)
-        for r in range(n_per):
-            ind = np.asarray(permutations[r])
-            y_perm = y.clone()[ind]
-            # compute accuracy
-            stats[r] = torch.sum(y_perm == y_hat) / y.shape[0]
-        sorted_stats = np.sort(stats)
-        p_val = np.sum(sorted_stats > accuracy.item()) / n_per
-        return p_val, accuracy
-
     def train_evaluate_epoch(self, loader, mode="train"):
-        """Train/Evaluate the model for one epocj and log the results."""
+        """
+        Train/Evaluate the model for one epoch using the C2ST approach,
+        but specifically designed for MNIST experiments with multiple network models.
+
+        Args:
+        - loader (DataLoader): DataLoader object to iterate through data.
+        - mode (str): Either "train" or "test". Determines how to run the model.
+
+        Returns:
+        - tuple: Aggregated loss and E-value using ONS betting strategy for the current epoch.
+        """
         aggregated_loss = 0
         e_val, tb_val, tb_val_ons = 1, 1, 1
         num_samples = len(loader.dataset)
+        num_pseudo_samples = None  # Placeholder for the number of pseudo samples. Will be initialized later.
+
         for i, (z, tau_z) in enumerate(loader):
             z = z.to(self.device)
             tau_z = tau_z.to(self.device)
-            num_pseudo_samples = tau_z.shape[-1]
+
+            if num_pseudo_samples is None:
+                num_pseudo_samples = tau_z.shape[-1]
+
             for i in range(num_pseudo_samples):
                 self.net[i] = self.net[i].train() if mode == "train" else self.net[i].eval()
+
                 out1 = self.net[i](z)
-                out2 = self.net[i](tau_z[...,i])
+                out2 = self.net[i](tau_z[..., i])
+
                 out = torch.concat((out1.clone(), out2.clone()))
+
                 labels = torch.concat((torch.ones((z.shape[0], 1)), torch.zeros((z.shape[0], 1)))).squeeze(1).long().to(
-                self.device)
+                    self.device)
+
                 loss = self.loss(out, labels)
+                aggregated_loss += loss
+
                 if mode == "train":
                     self.optimizer.zero_grad()
                     loss.backward()
                     self.optimizer.step()
-                # compute e-c2st and s-c2st
-                aggregated_loss += loss
+
+                # Compute the C2ST and other evaluation metrics
                 if mode == "test":
-                    p_val, acc = self.s_c2st(labels, out.detach(), n_per=100)
+                    p_val, acc = self.s_c2st(labels, out.detach())
                     results_tb = self.testing_by_betting(labels, out.detach())
                     tb_val_ons = results_tb[1]
 
-                    self.log(
-                        {f"{mode}_p-value_{i}": p_val,
-                         f"{mode}_accuracy": acc
-                        })
-            self.log({ f"{mode}_loss": aggregated_loss.item() / num_samples })
-        return aggregated_loss / (num_samples*num_pseudo_samples), tb_val_ons
+                    self.log({
+                        f"{mode}_p-value_{i}": p_val,
+                        f"{mode}_accuracy": acc
+                    })
+
+            self.log({
+                f"{mode}_loss": aggregated_loss.item() / num_samples
+            })
+
+        return aggregated_loss / (num_samples * num_pseudo_samples), tb_val_ons
+
 
 
--- trainer/trainerECRT.py
+++ trainer/trainerECRT.py
@@ -2,19 +2,37 @@ from trainer import Trainer
 import numpy as np
 import torch
 
+
 class TrainerECRT(Trainer):
     """Trainer class for the ECRT method."""
 
     def __init__(self, cfg, net, tau1, tau2, datagen, device, data_seed):
-        """Initializes the Trainer object with the provided configurations and parameters."""
+        """
+        Initializes the TrainerECRT object by extending the Trainer class.
+
+        Args:
+        (same as Trainer class)
+        """
         super().__init__(cfg, net, tau1, tau2, datagen, device, data_seed)
         self.loss = torch.nn.MSELoss(reduction='mean')
         self.integral_vector = torch.tensor(np.linspace(0, 1, 1001, endpoint=False)[1:]).to(device)
-        self.stb = [torch.ones(1000, device=device), torch.ones(1000, device=device), torch.ones(1000, device=device)]
+        self.stb = [torch.ones(1000, device=device) for _ in range(3)]
 
-    def ecrt(self,y,x, x_tilde, batches, mode):
-        # code adopted from https://github.com/shaersh/ecrt/
-        # iterate over batch sizes
+    def ecrt(self, y, x, x_tilde, batches, mode):
+        """
+        Implement the ECRT method.
+        Code adapted from https://github.com/shaersh/ecrt/
+
+        Args:
+        - y (torch.Tensor): Ground truth labels.
+        - x (torch.Tensor): Input samples.
+        - x_tilde (torch.Tensor): Perturbed samples.
+        - batches (list): List of batch sizes.
+        - mode (str): Either "train" or "test".
+
+        Returns:
+        - torch.Tensor: Test statistic.
+        """
         self.net = self.net.eval()
         total_samples = y.shape[0]
         test_stat = torch.nn.MSELoss(reduction='mean')
@@ -27,19 +45,19 @@ class TrainerECRT(Trainer):
             index_sets_seq = np.array_split(range(total_samples), num_chunks)
             stb.append(torch.ones(1000))
             for ind in index_sets_seq:
-                y_tb = y[ind]
-                x_tb = x[ind]
-                x_tilde_tb = x_tilde[ind]
+                y_tb, x_tb, x_tilde_tb = y[ind], x[ind], x_tilde[ind]
                 pred_tilde_tb = self.net(x_tilde_tb)
                 pred_tb = self.net(x_tb).detach()
+
                 q = test_stat(pred_tb.squeeze(), y_tb)
                 q_tilde = test_stat(pred_tilde_tb.squeeze(), y_tb)
                 wealth = torch.nn.Tanh()(q_tilde - q)
-                if mode=="test":
-                    self.stb[i] = self.stb[i].to(self.device)*(1 + self.integral_vector * wealth )
-                    stb[i] =  self.stb[i].clone()
+
+                if mode == "test":
+                    self.stb[i] = self.stb[i].to(self.device) * (1 + self.integral_vector * wealth)
+                    stb[i] = self.stb[i].clone()
                 else:
-                    stb[i] = stb[i].to(self.device)*(1 + self.integral_vector * wealth )
+                    stb[i] = stb[i].to(self.device) * (1 + self.integral_vector * wealth)
 
             st.append(stb[i].mean())
             i += 1
@@ -47,30 +65,44 @@ class TrainerECRT(Trainer):
         return st
 
     def train_evaluate_epoch(self, loader, mode="train"):
-        """Train/Evaluate the model for one epocj and log the results."""
-        aggregated_loss, loss_tilde = 0,0
-        num_samples = len(loader.dataset)
-        for i, (z, tau_z) in enumerate(loader):
+        """
+        Train/Evaluate the model for one epoch using the ECRT approach.
 
+        Args:
+        - loader (DataLoader): DataLoader object to iterate through data.
+        - mode (str): Either "train" or "test".
+
+        Returns:
+        - tuple: Aggregated loss and E-value for the current epoch.
+        """
+        aggregated_loss, loss_tilde = 0, 0
+        num_samples = len(loader.dataset)
+
+        for i, (z, tau_z) in enumerate(loader):
             z = z.to(self.device)
             features = z[:, :-1]
             target = z[:, -1]
             tau_z = tau_z.to(self.device)
-            if mode == "train":
-                self.net = self.net.train()
-                out = self.net(features)
-            else:
-                self.net = self.net.eval()
-                out = self.net(features)
-            loss =  self.loss(out.squeeze(), target)
-            aggregated_loss +=loss
-            y_tilde = self.net(tau_z[:,:-1]).detach()
+
+            self.net = self.net.train() if mode == "train" else self.net.eval()
+
+            out = self.net(features)
+            loss = self.loss(out.squeeze(), target)
+            aggregated_loss += loss
+
+            y_tilde = self.net(tau_z[:, :-1]).detach()
             loss_tilde += self.loss(y_tilde.squeeze(), target)
+
             if mode == "train":
                 self.optimizer.zero_grad()
                 loss.backward()
                 self.optimizer.step()
 
-            e_val = self.ecrt(target, features, tau_z[:,:-1], [2, 5, 10], mode)
-        self.log({ f"{mode}_loss": aggregated_loss.item()/(i+1),  f"{mode}_e-val": e_val.item(), f"{mode}_loss_tilde": loss_tilde.item()/(i+1)})
-        return aggregated_loss/num_samples, e_val
+            e_val = self.ecrt(target, features, tau_z[:, :-1], [2, 5, 10], mode)
+
+        self.log({
+            f"{mode}_loss": aggregated_loss.item() / (i + 1),
+            f"{mode}_e-val": e_val.item(),
+            f"{mode}_loss_tilde": loss_tilde.item() / (i + 1)
+        })
+        return aggregated_loss / num_samples, e_val
